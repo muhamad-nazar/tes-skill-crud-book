f1b24b130eb84ddcff952f9cd7dc5e511e2b5686
Muhamad Nazar <97673046+muhamad-nazar@users.noreply.github.com>
2024-01-30 13:31:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\PagesController;
-=======
->>>>>>> 8589cb02ed027bedbdeaa78b04ab2e9474b646a4
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,15 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
 //Home
 Route::get('/', [PagesController::class, 'index']);
 
 
 //Books & Categories
 Route::get('/categories', [PagesController::class, 'categories']);
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> 8589cb02ed027bedbdeaa78b04ab2e9474b646a4

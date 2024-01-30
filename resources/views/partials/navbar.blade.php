d006e13e393a3834e3389de8a058482fc24c01eb
Muhamad Nazar <muhamadnazar862@gmail.com>
2024-01-30 16:47:30 +0700

    Books Updated

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,7 +12,7 @@
           <li class="nav-item">
             <a class="nav-link @if ($link == "home")
                 actives
-            @endif" aria-current="page" href="#">Home</a>
+            @endif" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link @if ($link == "category")
